60564625fa81ba4d72dac1b8d17b90019f9fe7d2
vijay <vijay777013@gmail.com>
2020-01-19 02:02:38 +0530

    initial change

---
+++ blog_project/settings/prod.py
@@ -0,0 +1,58 @@
+from .base import *
+import dj_database_url
+from  decouple import config
+ 
+
+
+DEBUG = False
+#SECRET_KEY = '*z!vk$p+ep8pu@o6#pgivu%)#-bfnui(5@(azk2bv1)$wbvlcd'
+SECRET_KEY = config('SECRET_KEY')
+
+#production host
+ALLOWED_HOSTS = ['xtensionblog.herokuapp.com']
+
+
+AUTHENTICATION_BACKENDS += [
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
+    
+]
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfe2s7ni7l3jmq',
+        'USER': 'bszaqvyawctsvy',
+
+        'PASSWORD': '27d7bcd9d800a26cb5de6b9061768a44b6837310bb78619deabf26dbebc147ec',
+        'HOST': 'ec2-174-129-254-223.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+    
+}
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#Email Host 
+
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+LOGIN_REDIRECT_URL='post_list'
+PASSWORD_RESET_TIMEOUT_DAY = config('PASSWORD_RESET_TIMEOUT_DAY', default=1, cast=int)
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
+SOCIAL_AUTH_GITHUB_KEY = config('SOCIAL_AUTH_GITHUB_KEY')
+SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' #new
+
+
+
+
+#DATABASES = {'default': dj_database_url.config(default='postgres://bszaqvyawctsvy:27d7bcd9d800a26cb5de6b9061768a44b6837310bb78619deabf26dbebc147ec@ec2-174-129-254-223.compute-1.amazonaws.com:5432/dfe2s7ni7l3jmq')}
